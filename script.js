0f7506e8fc3e668518acdf664ae4e9e896630ca1
Kirill <razdor.2019@mail.ru>
2024-08-17 23:49:42 +0300

    learning js

--- script.js
+++ script.js
@@ -30,8 +30,11 @@ console.log(reverseAndNegate([1, -2, 5, 4]));
 //   return result;
 // }
 function calculate(arr) {
-    let newArr = arr.map(el => el % 2 == 0 ? el + 4 : el - 2).filter(el => el % 13 !== 0).reduce((a, b) => a + b)
-    return newArr
+  let newArr = arr
+    .map((el) => (el % 2 == 0 ? el + 4 : el - 2))
+    .filter((el) => el % 13 !== 0)
+    .reduce((a, b) => a + b);
+  return newArr;
 }
 console.log(calculate([15, 2, 3, 5, 6]));
 
@@ -46,3 +49,29 @@ function transfromUpvotes(arr) {
   return result;
 }
 console.log(transfromUpvotes(["10k", "2.3k", "5k", "32", "28.4k"]));
+const obj = [
+  {
+    name: "Petia",
+    age: 18,
+  },
+  {
+    name: "Pasha",
+    age: 22,
+  },
+  {
+    name: "Alex",
+    age: 15,
+  },
+];
+
+function getInfo(persons) {
+  let averageAge = 0;
+  persons.map((el) => (averageAge += el.age / persons.length));
+  let newArr = persons.reduce((max, obj) => (obj.age > max.age ? obj : max));
+  let oldestHuman = newArr.name;
+  let newObj = new Object();
+  newObj.oldestHuman = oldestHuman;
+  newObj.averageAge = Math.floor(averageAge);
+  return newObj;
+}
+console.log(getInfo(obj));
