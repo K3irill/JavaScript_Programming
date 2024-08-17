3be3f0c7b0d285a4247a14e4175c37b6c2e9a182
Kirill <razdor.2019@mail.ru>
2024-08-17 23:03:09 +0300

    learning js

--- script.js
+++ script.js
@@ -10,26 +10,29 @@ function reverseAndNegate(arr) {
 }
 console.log(reverseAndNegate([1, -2, 5, 4]));
 
+// function calculate(arr) {
+//   let ab = [];
+//   let newArr = [];
+//   let result = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       ab.push(arr[i] + 4);
+//     } else ab.push(arr[i] - 2);
+//   }
+//   ab.map((el) => {
+//     if (el % 13 !== 0) {
+//       newArr.push(el);
+//     }
+//   });
+//   newArr.map((el) => {
+//     result += el;
+//   });
+//   return result;
+// }
 function calculate(arr) {
-  let ab = [];
-  let newArr = [];
-  let result = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      ab.push(arr[i] + 4);
-    } else ab.push(arr[i] - 2);
-  }
-  ab.map((el) => {
-    if (el % 13 !== 0) {
-      newArr.push(el);
-    }
-  });
-  newArr.map((el) => {
-    result += el;
-  });
-  return result;
+    let newArr = arr.map(el => el % 2 == 0 ? el + 4 : el - 2).filter(el => el % 13 !== 0).reduce((a, b) => a + b)
+    return newArr
 }
-
 console.log(calculate([15, 2, 3, 5, 6]));
 
 function transfromUpvotes(arr) {
