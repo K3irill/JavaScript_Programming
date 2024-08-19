2a33cf6618acd016f0d748c2b707f29988de2eff
K3rill <razdor.2019@mail.ru>
2024-08-19 14:58:40 +0300

    working with spread

---
+++ spread.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function multiplyThreeNum(a, b, c) {
+  return a * b * c;
+}
+const arrNums = [6, 5, 2];
+
+console.log(multiplyThreeNum(...arrNums));//console.log(multiplyThreeNumbers(nums[0], nums[1], nums[2]))
+
+const firstArr = ["lorem", "first", "secnod", "end"];
+const secondArr = [...firstArr, "good job", null];
+
+console.log(secondArr);
+
+const person = {
+  name: "Mike",
+  age: 20,
+};
+
+const user = {
+    ...person,
+    language: ['English', 'Spanish', 'German'],
+    country: 'USA',
+};
+
+console.log(user);
