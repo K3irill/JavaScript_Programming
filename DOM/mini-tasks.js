521025f5d5ce0a8ca53a2a70f87563a11147c1ce
K3rill <razdor.2019@mail.ru>
2024-08-20 16:39:25 +0300

    work with dom

--- DOM/mini-tasks.js
+++ DOM/mini-tasks.js
@@ -1,2 +1,16 @@
 "use strict";
 
+// task 1
+const dataSayHiElement = document.querySelector("[data-say-hi]");
+const attributeDataSayHiEl = dataSayHiElement.getAttribute("data-say-hi");
+console.log(attributeDataSayHiEl);
+
+//task 2
+const iyonchiText = document.querySelector('.task__list *:last-child')
+console.log(iyonchiText);
+
+//task 3
+const likeListElements = document.querySelectorAll('.like')
+console.log(likeListElements);
+
+//task 4
