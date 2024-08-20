d7b4a1057da672dd4be5b05c32529f6150949a9c
K3rill <razdor.2019@mail.ru>
2024-08-20 14:48:31 +0300

    working with dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -23,13 +23,51 @@ console.log(childNodes);
 
 console.log(bodyElement.hasChildNodes);
 
-
-
 for (let node of childNodes) {
   console.log(node);
 }
 
-
 const children = bodyElement.children;
 
 console.log(children);
+
+//---Static collection
+const listStatic = document.querySelectorAll(".container_item");
+
+//---Live collection
+const listLive = document.getElementsByClassName("container_item");
+
+console.log(listStatic);
+console.log(listLive);
+
+//---- closest
+const elementt = document.querySelector(".container_link");
+const parentList = elementt.closest(".container_item");
+
+console.log(parentList);
+
+//----- innerHTML
+const textElement = document.querySelector(".title");
+
+const textElementContent = textElement.innerHTML;
+console.log(textElementContent);
+
+textElement.innerHTML = `<p>${textElementContent}</p>
+ <p><span style='color:yellow; text-shadow: 1px 1px 1px black' >hey</span> what's up</p>`;
+
+//----- textContent
+ const textElement2 = document.querySelector(".title");
+
+const textElementContent2 = textElement.textContent;
+console.log(textElementContent2);
+
+textElement.textContent = "hey how's it going on";
+
+// change comment
+
+const getComment = textElement.nextSibling;
+console.log(getComment);
+console.log(getComment.data);
+
+getComment.data =  "uppss it was сhanged";
+console.log(getComment.data);
--- DOM/getElement.js
+++ DOM/getElement.js
@@ -1,1 +1,21 @@
 'use strict'
+
+//  getElementById
+
+const elemId = document.getElementById('simpleLink')
+console.log(elemId);
+
+//  getElementsByTagName
+
+const elemsTag = document.getElementsByTagName('li')
+console.log(elemsTag);
+
+// getElementsByClassName
+
+const elemsClass = document.getElementsByClassName('title')
+console.log(elemsClass);
+
+//  getElementsByName
+
+const elemsName = document.getElementsByName('subtitle')
+console.log(elemsName);
