616e04917c0375b588b85d1c0a54b6fcd0b51986
K3rill <razdor.2019@mail.ru>
2024-08-21 20:05:39 +0300

    a few basic math functions

--- simple_calculator/math.js
+++ simple_calculator/math.js
@@ -1,1 +1,15 @@
-'use strict'
+"use strict";
+
+function sum(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
