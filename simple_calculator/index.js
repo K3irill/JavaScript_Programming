bfa1999cda4e641695e9c3f571e606c432b5080a
K3rill <razdor.2019@mail.ru>
2024-08-21 22:16:30 +0300

    add interaction with dom

---
+++ simple_calculator/index.js
@@ -0,0 +1,20 @@
+"use scrict";
+
+const inputANode = document.querySelector(".js-input-a");
+const inputBNode = document.querySelector(".js-input-b");
+const selectOperationNode = document.querySelector(".js-select-operation");
+const btnResultNode = document.querySelector(".js-btn-result");
+const outputNode = document.querySelector(".js-output");
+
+btnResultNode.addEventListener("click", function () {
+  const a = +inputANode.value;
+  const b = +inputBNode.value;
+  const operation = selectOperationNode.value;
+  const result = calculator({
+    a,
+    b,
+    operation,
+  });
+
+  outputNode.innerHTML = `<h2>Ответ: ${result}</h2>`;
+});
