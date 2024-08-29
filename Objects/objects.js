6d0b301dddf4668acc7ae73fba6711b4e9fdbab6
K3rill <razdor.2019@mail.ru>
2024-08-29 22:50:56 +0300

    getters and setters

--- Objects/objects.js
+++ Objects/objects.js
@@ -47,3 +47,113 @@ console.log("error" in newObj);
 for (let i in newObj) {
     console.log(`for in: ${i}`);
 }
+
+//
+let name = "Jack";
+let surname = "Jamberson";
+let sex = "male";
+let age = 20;
+
+//ES5
+var person = {
+    name: name,
+    sex: sex,
+    age: age,
+    greet: function () {
+        console.log(`Hello ${this.name}`);
+    },
+};
+
+Object.defineProperty(person, "password", {
+    get: function () {
+        return this.name + this.age;
+    },
+});
+console.log(person);
+
+//ES6
+var person2 = {
+    name,
+    surname,
+    sex,
+    age,
+    greet() {
+        console.log(`Hello ${this.name}`);
+    },
+    get password() {
+        return this.name + this.age;
+    },
+
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    },
+
+    // set fullName(value) {
+    //     [this.name, this.surname] = value.split(" ");
+    // },
+
+    set fullName(value) {
+        if (value.length < 4) {
+            alert("Имя слишком короткое, должно быть более 4 символов");
+            return;
+        }
+        this._name = value;
+    },
+};
+
+// person2.fullName = "Alice Cooper";
+console.log(person2.fullName);
+
+//
+
+person.name;
+person["name"];
+
+let firstName = "name";
+person[firstName];
+
+// person = {
+//     [firstName]: "Max",
+// };
+
+console.log(person);
+
+//spaceship
+
+let spaceship = {
+    passengers: null,
+    telescope: {
+        yearBuilt: 2018,
+        model: "91031-XLT",
+        focalLength: 2032,
+    },
+    crew: {
+        captain: {
+            name: "Sandra",
+            degree: "Computer Engineering",
+            encourageTeam() {
+                console.log("We got this!");
+            },
+            "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+        },
+    },
+    engine: {
+        model: "Nimbus2000",
+    },
+    nanoelectronics: {
+        computer: {
+            terabytes: 100,
+            monitors: "HD",
+        },
+        "back-up": {
+            battery: "Lithium",
+            terabytes: 50,
+        },
+    },
+};
+let capFave = spaceship.crew.captain["favorite foods"][0];
+spaceship.passengers = [
+    { name: "Kail", age: 20 },
+    { name: "Kira", age: 19 },
+];
+let firstPassenger = spaceship.passengers[0];
