7f2cb646b4410bc59bfbd1a67ea19f2cf1815f97
K3rill <razdor.2019@mail.ru>
2024-08-31 13:42:38 +0300

    finish

--- carousel/script.js
+++ carousel/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+new Swiper(".card-wrapper", {
+    loop: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets:true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    },
+});
